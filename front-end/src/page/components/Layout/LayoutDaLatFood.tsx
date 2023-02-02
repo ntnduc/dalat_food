36c56223fa0b5474b99d8c3c5563463a8451d581
Nguyễn Trần Nhật Đức <68223190+ntnduc@users.noreply.github.com>
2023-02-03 00:19:49 +0700

    <feature>: update layout

--- front-end/src/page/components/Layout/LayoutDaLatFood.tsx
+++ front-end/src/page/components/Layout/LayoutDaLatFood.tsx
@@ -1,16 +1,25 @@
-import {Breadcrumb, Layout, Menu, theme} from 'antd';
+import {Avatar, Badge, Breadcrumb, Layout, Menu, theme} from 'antd';
 import React from 'react';
 const {Header, Content, Footer} = Layout;
+import './style/layout-style.scss';
+
+import { ShoppingOutlined, UserOutlined } from '@ant-design/icons';
+
+import LOGO from '../../../logo.svg';
 
 const LayoutDaLatFood = () => {
     const {
         token: {colorBgContainer},
     } = theme.useToken();
 
+    const onLogin = () => {
+        console.log('login');
+    };
+
     return (
-        <Layout>
-            <Header style={{position: 'sticky', top: 0, zIndex: 1, width: '100%'}}>
-                <div
+        <Layout className='layout-dalat-food'>
+            <Header style={{position: 'sticky', top: 0, zIndex: 1, width: '100%'}} className='custom-header'>
+                <div    
                     style={{
                         float: 'left',
                         width: 120,
@@ -19,22 +28,26 @@ const LayoutDaLatFood = () => {
                         background: 'rgba(255, 255, 255, 0.2)',
                     }}
                 />
-                <Menu
-                    theme="dark"
-                    mode="horizontal"
-                    defaultSelectedKeys={['2']}
-                    items={new Array(3).fill(null).map((_, index) => ({
-                        key: String(index + 1),
-                        label: `nav ${index + 1}`,
-                    }))}
-                />
+                <div className='logo'>
+                    <img src={LOGO} alt="Da Lat Food Logo" />
+                </div>
+                <div className='content-right'>
+                    <div className='user-cart'>
+                        <Badge count={2} className='user-cart-count'>
+                            <ShoppingOutlined style={{fontSize: 30, opacity: 0.4}}/>
+                        </Badge>
+                        Giỏ Hàng
+                    </div>
+                    <div className='user-login' onClick={onLogin}>
+                        <Avatar className='avatar-user'
+                            size={30}
+                            icon={<UserOutlined />} />
+                        Đăng Nhập
+                    </div>
+                </div>
+
             </Header>
             <Content className="site-layout" style={{padding: '0 50px'}}>
-                <Breadcrumb style={{margin: '16px 0'}}>
-                    <Breadcrumb.Item>Home</Breadcrumb.Item>
-                    <Breadcrumb.Item>List</Breadcrumb.Item>
-                    <Breadcrumb.Item>App</Breadcrumb.Item>
-                </Breadcrumb>
                 <div style={{padding: 24, minHeight: 380, background: colorBgContainer}}>Content</div>
             </Content>
             <Footer style={{textAlign: 'center'}}>Ant Design ©2023 Created by Ant UED</Footer>
