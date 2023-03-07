cc2f8f436ef0bc2380676ab08de386f5f41a7df1
Nguyễn Trần Nhật Đức <68223190+ntnduc@users.noreply.github.com>
2023-03-08 01:20:08 +0700

    <fix>: serivce production

--- back-end/DaLatFood/DaLatFood.Application/Production/Service/ProductService.cs
+++ back-end/DaLatFood/DaLatFood.Application/Production/Service/ProductService.cs
@@ -2,9 +2,8 @@ namespace DaLatFood.Application.Production.Service;
 
 public class ProductService : IProductService
 {
-    private readonly IProductRepository
     public Task<object> GetProduction(Guid id)
     {
-        
+        throw new NotImplementedException();
     }
 }
