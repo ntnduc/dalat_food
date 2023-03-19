5f16a65377440188017cb429ed1e09dd0945f11e
Nguyễn Trần Nhật Đức <68223190+ntnduc@users.noreply.github.com>
2023-03-19 16:23:33 +0700

    Update ProductController.cs

--- back-end/DaLatFood/DaLatFood.WebApi/Controllers/Product/ProductController.cs
+++ back-end/DaLatFood/DaLatFood.WebApi/Controllers/Product/ProductController.cs
@@ -1,28 +1,18 @@
-using DaLatFood.Application.Common;
 using DaLatFood.Application.Production.Dto;
 using DaLatFood.Application.Production.Service;
-using DaLatFood.Domain.Core.Response;
-using Microsoft.AspNetCore.Authorization;
+using DaLatFood.Domain.Product.Entity;
+using DaLatFood.WebApi.Base;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DaLatFood.WebApi.Controllers.Product;
 
 [ApiController]
 [Route("/api/product")]
-public class ProductController : ControllerBase
+public class ProductController : CrudControllerBase<Production, ProductListDto,ProductListDto,ProductListDto,ProductListDto>
 {
-    private IProductService _productService;
-
-    public ProductController(IProductService productService)
+    private readonly IProductService _productService;
+    public ProductController(IProductService productService) : base(productService)
     {
         _productService = productService;
-    }
-
-    [HttpGet("list")]
-    [AllowAnonymous]
-    public async Task<ApiResponse<PaginatedList<ProductListDto>>> GetListProduct(CancellationToken cancellationToken = default (CancellationToken))
-    {
-        var listResult = await _productService.GetListAsync(cancellationToken);
-        return ApiResponse<PaginatedList<ProductListDto>>.Ok(listResult);
     }
 }
