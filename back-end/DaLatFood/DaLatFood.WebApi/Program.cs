8ae0ccd63ccecacf4201f60766a1ab781a21a5ec
Nguyễn Trần Nhật Đức <68223190+ntnduc@users.noreply.github.com>
2023-03-23 23:24:36 +0700

    <feat>: add proxy api

--- back-end/DaLatFood/DaLatFood.WebApi/Program.cs
+++ back-end/DaLatFood/DaLatFood.WebApi/Program.cs
@@ -1,11 +1,11 @@
 using DaLatFood.Application;
 using DaLatFood.Infrastructure;
 using DaLatFood.Infrastructure.Data;
-using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
 var connectionString = builder.Configuration.GetConnectionString("Default");
 // Add services to the container.
 builder.Services
@@ -20,21 +20,25 @@ builder.Services.AddSwaggerGen(a =>
     a.SwaggerDoc("v1", new OpenApiInfo { Title = "Da Lat Food", Version = "v1" });
     a.CustomSchemaIds(i => i.FullName);
 });
-
+//Db context
 builder.Services.AddDbContext<ApplicationDbContext>(options => { options.UseSqlServer(connectionString); });
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseRouting();
+app.UseEndpoints(endPoints => endPoints.MapControllers());
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseSpa(spa =>
+    {
+        spa.UseProxyToSpaDevelopmentServer("https://localhost:3000");
+    });
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+// app.UseHttpsRedirection();
 
 app.MapControllers();
 
